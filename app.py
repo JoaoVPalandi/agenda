297899f6d174871144261faf3bbc15e65a9467f9
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-02-02 16:45:21 -0300

    feat: Salvando as tarefas do banco SQLite3

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+
 from flask import Flask, render_template, request
+
 from models.tarefa import Tarefa
 
 app = Flask(__name__)
@@ -7,14 +9,15 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', titulo='Home')
 
-@app.route('/agenda', methods=['GET', 'POST'])
+@app.route('/agenda', methods=['GET','POST'])
 def agenda():
     tarefa = None
-    if request.method == 'POST':
-        titulo_tarefa = request.form['titulo-tarefa']
-        data_conclusao = request.form['data-conclusao']
-        tarefa = Tarefa(titulo_tarefa, data_conclusao)
 
+    if request.method == 'POST':
+     titulo_tarefa = request.form['titulo-tarefa']
+     data_conclusao = request.form['data-conclusao']
+     tarefa = Tarefa(titulo_tarefa, data_conclusao)
+     tarefa.salvar_tarefa()
     return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
 
 @app.route('/ola')
