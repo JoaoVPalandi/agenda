ac1278e4718e2c177b6650d86eea2424c5963b8a
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-02-03 11:32:05 -0300

    style: estilização da pagina agenda python

--- models/tarefa.py
+++ models/tarefa.py
@@ -3,11 +3,11 @@ from models.database import Database
 
 class Tarefa:
     def __init__(self: Self, titulo_tarefa: str, data_conclusao =None)-> None:
-        self.titulo: str = titulo_tarefa
+        self.titulo_tarefa: str = titulo_tarefa
         self.data_conclusao: str  = data_conclusao 
         
     def salvar_tarefa(self: Self)-> None:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
-            params: tuple = (self.titulo, self.data_conclusao)
+            params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
