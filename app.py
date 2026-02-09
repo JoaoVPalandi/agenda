d7589707d332b811201becc136bc32129d7a38a5
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-02-09 10:40:49 -0300

    refact: declarando explicitamente os tipos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from models.tarefa import Tarefa
 app = Flask(__name__)
 
@@ -23,7 +23,9 @@ def agenda():
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
     tarefa.excluir_tarefa()
-    return
+    # return render_template('agenda.html', titulo="Agenda",
+    # tarefa=tarefas)
+    return redirect(url_for('agenda'))
 
 @app.route('/ola')
 def ola_mundo():
--- models/database.py
+++ models/database.py
@@ -1,38 +1,51 @@
-
 from sqlite3 import Connection, connect, Cursor
-from typing import Any
+from typing import Any, Optional, Self, Type
+from types import TracebackType
+import traceback
 
 class Database:
-    def __init__(self, db_name: str) -> None :
+    def __init__(self, db_name: str) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
-        self.executar('''
+        self.executar("""
         CREATE TABLE IF NOT EXISTS tarefas (
          id INTEGER PRIMARY KEY AUTOINCREMENT,
          titulo_tarefa TEXT NOT NULL,
          data_conclusao TEXT);
-        ''')
+        """)
 
     def executar(self, query: str, params: tuple = ()) -> Cursor:
         self.cursor.execute(query, params)
         self.connection.commit()
         return self.cursor
-    
+
     def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
-    
+
     def close(self) -> None:
         self.connection.close()
 
     # Métodos para o gerenciamento de contexto
 
-    #Método de entrada do contexto 
-    def __enter__(self):
+    # Método de entrada do contexto
+    def __enter__(self) -> Self:
         return self
 
-    # Método de saída do contexto 
-    def __exit__(self, exc_type, exc_value, traceback):
+    # Método de saída do contexto
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        tb: Optional[TracebackType],
+    ) -> None:
+        if exc_type is not None:
+            print("Exceção capiturar no contexto: ")
+            print(f"Tipo: {exc_type.__name__}")
+            print(f"Mensagem: {exc_value}")
+            print("Traceback completo:")
+            traceback.print_tb(tb)
+
         self.close()
 
 
