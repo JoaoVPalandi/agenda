a93876a35549a918f242b8af63cc0674ecf007dc
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-02-23 12:52:39 -0300

    feat: adicionar campo 'concluida' na tabela de tarefas e atualizar lógica de conclusão

--- models/tarefa.py
+++ models/tarefa.py
@@ -6,36 +6,37 @@ class Tarefa:
     """
         Classe Para representar tarefa, com metodos para salvar, obter, excluir e  atualizar tarefas com um banco de dados usando a classe Database.
     """
-    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa:Optional[int] = None,)-> None:
+    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str]= None, id_tarefa: Optional[int] = None, concluida: Optional[int] = 0) -> None:
         self.titulo_tarefa: Optional[str] = titulo_tarefa
-        self.data_conclusao: Optional[str]  = data_conclusao
+        self.data_conclusao: Optional[str] = data_conclusao
         self.id_tarefa: Optional[int] = id_tarefa
+        self.concluida: Optional[int] = concluida
 
     @classmethod
     def id(cls, id: int) -> Self:
         with Database() as db:
-            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
+            query: str = 'SELECT titulo_tarefa, data_conclusao, concluida FROM tarefas WHERE id = ?;'
             params: tuple = (id,)
             resultado = db.buscar_tudo(query, params)
             print(resultado)
 
             #desenpacotamento de coleção
-            [[titulo, data]] = resultado
+            [[titulo, data, concluida]] = resultado
 
-        return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
+        return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data, concluida=concluida)
         
     def salvar_tarefa(self: Self)-> None:
         with Database() as db:
-            query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
-            params: tuple = (self.titulo_tarefa, self.data_conclusao)
+            query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao, concluida) VALUES (?, ?, ?);"
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.concluida)
             db.executar(query, params)
 
     @classmethod
     def obter_tarefas(cls) -> list[Self]:
         with Database() as db:
-            query: str = 'SELECT titulo_tarefa, data_conclusao, id FROM tarefas;'
+            query: str = 'SELECT titulo_tarefa, data_conclusao, id, concluida FROM tarefas;'
             resultados: list[Any] = db.buscar_tudo(query)
-            tarefas: list[Any] = [cls(titulo, data, id) for titulo, data, id in resultados]
+            tarefas: list[Any] = [cls(titulo, data, id, concluida) for titulo, data, id, concluida in resultados]
             return tarefas
     
     def excluir_tarefa(self) -> Cursor:
@@ -47,14 +48,14 @@ class Tarefa:
     
     def atualizar_tarefas(self) -> Cursor:
            with Database() as db:
-            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
-            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ?, concluida = 0 WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa, self.concluida)
             resultado: Cursor = db.executar(query, params)
             return resultado
            
     def concluir_tarefa(self) -> Cursor:
         with Database() as db:
-            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ?, concluida = 1 WHERE id = ?;'
             params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
             resultado: Cursor = db.executar(query, params)
             return resultado
